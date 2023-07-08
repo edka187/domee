b094d29b1eb7b56845dff4f48a30bd2700410674
Edgar <edgarkessler@outlook.com>
2023-07-08 22:24:59 +0200

    Initial commit

---
+++ src/main/java/eu/eddo/domee/Domee.java
@@ -0,0 +1,17 @@
+package eu.eddo.domee;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Domee extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
